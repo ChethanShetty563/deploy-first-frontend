6b88a35b3f140cd4abf006b103cd3bb43fba7062
ChethanShetty563 <chethan563@gmail.com>
2022-10-02 15:40:28 +0530

    making chnages

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
       <div>
         <input
           type="text"
-          placeholder="Name..."
+          placeholder="Name"
           onChange={(event) => {
             setName(event.target.value);
           }}
